0d44fe5755bdc012d29de95fdbae55c0023db0da
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-05-25 10:56:28 -0400

    React functions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@
 import './App.css';
 
 function App() {
+  let input;
+  let palindrome = [];
+  const handleInput = (e) => {
+    input = e.target.value;
+  };
+  const reverseWord = () => {
+    for (let i = (value.length - 1); i > -1 ; i--) {
+      const elem = value[i];
+      palindrome.push(elem);
+    }
+  };
+  const concatWord = () => {
+    palindrome = palindrome.join('');
+  };
+  const confirmMatch = () => {
+    if (input === palindrome) {
+      return;
+    }
+  };
+  const clearPalindrome = () => {
+    palindrome = [];
+  }
+  const checkIfPalindrome = (value) => {
+    reverseWord(value);
+    concatWord();
+    confirmMatch();
+    clearPalindrome();
+  }
   return (
     <div className="App">
       <div className="main">
         <div className="inputArea">
-          <input />
-          <button>Submit</button>
+          <input type="text" onChange={(e) => handleInput(e)}/>
+          <button onClick={() => checkIfPalindrome()}>Submit</button>
         </div>
       <div>yaaaayyyy</div>
       </div>
